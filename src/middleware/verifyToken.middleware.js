5ce3fd2325b58e89c18a6d9e69e7cf15fcef58f9
Ghayoor Hussain <92637639+Ghauoor@users.noreply.github.com>
2025-03-24 15:23:17 +0500

    add middleware to verify JWT access tokens

---
+++ src/middleware/verifyToken.middleware.js
@@ -0,0 +1,23 @@
+import jwt from "jsonwebtoken";
+import { ACCESS_TOKEN_SECRET } from "../config/config";
+
+export const verifyToken = async (req, reply, done) => {
+    try {
+        const authHeader = req.headers['authorization'];
+
+        if (!authHeader || !authHeader.startsWith('Bearer ')) {
+            return reply.status(401).send({ message: 'Access Token is Required' });
+        }
+
+        const token = authHeader.split(' ')[1];
+        const decoded = jwt.verify(token, ACCESS_TOKEN_SECRET);
+
+        req.user = decoded;
+
+        return true
+
+
+    } catch (error) {
+        return reply.status(500).send({ message: 'Internal server error', error: error.message });
+    }
+}
